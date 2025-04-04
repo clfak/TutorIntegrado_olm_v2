380767628c4b6cd64d762f7136446235eb4a557d
Marcelo Trivinos <marceloto260@gmail.com>
2025-04-03 21:10:26 -0300

    Fix formulary equations render

--- src/components/challenge/MathDisplay.tsx
+++ src/components/challenge/MathDisplay.tsx
@@ -5,9 +5,20 @@ import TeX from "@matejmazur/react-katex";
 import "katex/dist/katex.min.css";
 
 const MathDisplay = ({ description, mathExpression, image }) => {
+  const renderDescription = text => {
+    // Expresión regular para encontrar \text{contenido}
+    const texRegex = /\\text\{([^}]*)\}/g;
+
+    // Si no contiene \text{}, devuelve el texto normal
+    if (!texRegex.test(text)) {
+      return <Text mb={4}>{text}</Text>;
+    }
+    return <TeX>{text}</TeX>;
+  };
+
   return (
     <Box p={4} borderWidth="1px" borderRadius="lg" boxShadow="sm" maxW="400px">
-      <Text mb={4}>{description}</Text>
+      <Box mb={4}>{renderDescription(description)}</Box>
       {image ? <Image src={image}></Image> : <TeX>{mathExpression}</TeX>}
     </Box>
   );
